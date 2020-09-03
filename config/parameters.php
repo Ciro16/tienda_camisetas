1612042fd8195ef10333c6201f268316340b534d
Ciro Perez <cirokip0@gmail.com>
2020-09-03 15:11:13 -0400

    Proyecto tienda de camisetas

---
+++ config/parameters.php
@@ -0,0 +1,8 @@
+<?php
+//$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/';
+$url = 'http://localhost/proyectos/tienda_ropa2/';
+
+define("base_url", $url);
+define("controller_default", "productoController");
+define("action_default", "index");
+
---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<p>Desarrollado por Ciro Pérez Iglesias &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
