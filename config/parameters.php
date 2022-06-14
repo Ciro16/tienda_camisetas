3739294c82c0414048165d371bdd0e50e1b2ef0d
Ciro Perez <ciro@bmiahorro.com>
2022-06-14 20:16:15 +0200

    Actualizacion imagenes y configuracion

--- config/parameters.php
+++ config/parameters.php
@@ -1,8 +1,6 @@
 <?php
-//$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/';
-$url = 'http://localhost/proyectos/tienda_ropa2/';
+$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/tienda_camisetas/';
 
 define("base_url", $url);
 define("controller_default", "productoController");
 define("action_default", "index");
-
--- database.sql
+++ database.sql
@@ -1,65 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 14-06-2022 a las 20:07:46
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 CREATE DATABASE tienda_master;
 USE tienda_master;
 
-CREATE TABLE usuarios(
-    id              int(255) auto_increment not null,
-    nombre          varchar(100) not null,
-    apellidos       varchar(255),
-    email           varchar(255) not null,
-    password        varchar(255) not null,
-    rol             varchar(20),
-    imagen          varchar(255),
-CONSTRAINT pk_usuarios PRIMARY KEY(id),
-CONSTRAINT uq_email UNIQUE(email)  
-)ENGINE=InnoDb;
+--
+-- Base de datos: `tienda_master`
+--
 
-INSERT INTO usuarios VALUES(NULL, 'Admin', 'Admin', 'admin@admin.com', 'contraseña', 'admin', null);
+-- --------------------------------------------------------
 
-CREATE TABLE categorias(
-    id              int(255) auto_increment not null,
-    nombre          varchar(100) not null,
-    CONSTRAINT pk_categorias PRIMARY KEY(id) 
-)ENGINE=InnoDb;
+--
+-- Estructura de tabla para la tabla `categorias`
+--
 
-INSERT INTO categorias VALUES(null, 'Tirantes');
-INSERT INTO categorias VALUES(null, 'Manga larga');
-INSERT INTO categorias VALUES(null, 'Sudaderas');
+CREATE TABLE `categorias` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE productos(
-    id              int(255) auto_increment not null,
-    categoria_id    int(255) not null,
-    nombre          varchar(100) not null,
-    descripcion     text,
-    precio          float(100,2) not null,
-    stock           int(255) not null,
-    oferta          varchar(2),
-    fecha           date not null,
-    imagen          varchar(255),
-    CONSTRAINT pk_categorias PRIMARY KEY(id),
-    CONSTRAINT fk_producto_categoria FOREIGN KEY(categoria_id) REFERENCES categorias(id)
-)ENGINE=InnoDb;
+--
+-- Volcado de datos para la tabla `categorias`
+--
 
+INSERT INTO `categorias` (`id`, `nombre`) VALUES
+(1, 'Tirantes'),
+(2, 'Manga larga'),
+(3, 'Sudaderas'),
+(4, 'Manga Corta');
 
-CREATE TABLE pedidos(
-    id              int(255) auto_increment not null,
-    usuario_id      int(255) not null,
-    provincia       varchar(100) not null,
-    localidad       varchar(100) not null,
-    direccion       varchar(255) not null,
-    coste           float(200,2) not null,
-    estado          varchar(20) not null,
-    fecha           date,
-    hora            time,
-    CONSTRAINT pk_pedidos PRIMARY KEY(id),
-    CONSTRAINT fk_pedido_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
-)ENGINE=InnoDb;
+-- --------------------------------------------------------
 
-CREATE TABLE lineas_pedidos(
-    id              int(255) auto_increment not null,
-    pedido_id       int(255) not null,
-    producto_id     int(255) not null,
-    unidades        int(255) not null,
-    CONSTRAINT pk_lineas_pedidos PRIMARY KEY(id),
-    CONSTRAINT fk_linea_pedido FOREIGN KEY(pedido_id) REFERENCES pedidos(id),
-    CONSTRAINT fk_linea_producto FOREIGN KEY(producto_id) REFERENCES productos(id)
-)ENGINE=InnoDb;
+--
+-- Estructura de tabla para la tabla `lineas_pedidos`
+--
+
+CREATE TABLE `lineas_pedidos` (
+  `id` int(255) NOT NULL,
+  `pedido_id` int(255) NOT NULL,
+  `producto_id` int(255) NOT NULL,
+  `unidades` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id` int(255) NOT NULL,
+  `usuario_id` int(255) NOT NULL,
+  `provincia` varchar(100) NOT NULL,
+  `localidad` varchar(100) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `coste` float(200,2) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(255) NOT NULL,
+  `categoria_id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `precio` float(100,2) NOT NULL,
+  `stock` int(255) NOT NULL,
+  `oferta` varchar(2) DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `categoria_id`, `nombre`, `descripcion`, `precio`, `stock`, `oferta`, `fecha`, `imagen`) VALUES
+(1, 4, 'Camiseta Metallica', 'Excelente camiseta de Metallica', 30.00, 4, NULL, '2022-06-14', 'metallica.jpg'),
+(2, 4, 'Camiseta Rayas', 'Linda camiseta de rayas azules', 13.00, 3, NULL, '2022-06-14', 'rayasazules.jpg'),
+(3, 3, 'Sudadera Negra', 'Sudadera lista para el frío!', 16.00, 3, NULL, '2022-06-14', 'sudadera_negra.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` varchar(20) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `rol`, `imagen`) VALUES
+(1, 'Admin', 'Admin', 'admin@admin.com', '$2y$04$6pW8LbyMsXs2GJsIClTUleQEv3p2ALGwwcLlGr.Fotvp.yxgQGN9O', 'admin', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `lineas_pedidos`
+--
+ALTER TABLE `lineas_pedidos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_linea_pedido` (`pedido_id`),
+  ADD KEY `fk_linea_producto` (`producto_id`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pedido_usuario` (`usuario_id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_producto_categoria` (`categoria_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `lineas_pedidos`
+--
+ALTER TABLE `lineas_pedidos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `lineas_pedidos`
+--
+ALTER TABLE `lineas_pedidos`
+  ADD CONSTRAINT `fk_linea_pedido` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`id`),
+  ADD CONSTRAINT `fk_linea_producto` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`);
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `fk_pedido_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
